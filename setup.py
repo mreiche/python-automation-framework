7cc1bf208f68e2ebcfa6902ee6d896cb97effb47
Mike Reiche <mike.reiche@ewerk.com>
2023-04-30 10:21:21 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description="Automation framework for the WebDriver API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.0.7",
+    version="0.0.8",
     url="https://github.com/mreiche/python-automation-framework",
     author="Mike Reiche",
     packages=["paf"],
