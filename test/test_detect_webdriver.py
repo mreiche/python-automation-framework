1c5dc19f8159f4230120b588899f1521f0a45366
Mike Reiche <mike@reiche.world>
2024-07-20 12:49:34 +0200

    Re-enabled test

--- test/test_detect_webdriver.py
+++ test/test_detect_webdriver.py
@@ -15,15 +15,19 @@ def test_detect_webdriver():
     assert result is True
 
 
-@pytest.mark.skip(reason="Does not work in test image")
+#@pytest.mark.skip(reason="Does not work in test image")
 def test_hide_webdriver():
     # https://stackoverflow.com/questions/53039551/selenium-webdriver-modifying-navigator-webdriver-flag-to-prevent-selenium-detec
     request = WebDriverRequest("non-automated")
     request.browser = "chrome"
     options = ChromeOptions()
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+
     request.options = options
     webdriver = create_webdriver(request)
+    webdriver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
     webdriver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     result = webdriver.execute_script("return navigator.webdriver")
     assert result is False
