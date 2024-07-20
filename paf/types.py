de1d159a35839e9fd064be122b8327ec37e50951
Mike Reiche <mike@reiche.world>
2024-07-20 14:39:19 +0200

    Improve Component code coverage

--- paf/types.py
+++ paf/types.py
@@ -8,5 +8,6 @@ Mapper = Callable[[T], R]
 Number = float | int
 Consumer = Callable[[T], None]
 COMPONENT = TypeVar("COMPONENT")
+SUB_COMPONENT = TypeVar("SUB_COMPONENT")
 PAGE = TypeVar("PAGE")
 ACTUAL_TYPE = TypeVar("ACTUAL_TYPE")
