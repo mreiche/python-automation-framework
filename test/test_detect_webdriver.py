f035348e25f48a0b0fa80ef2ea107c279509be12
Mike Reiche <mike@reiche.world>
2024-07-20 11:45:03 +0200

    Stash commit before build

--- test/test_detect_webdriver.py
+++ test/test_detect_webdriver.py
@@ -17,12 +17,14 @@ def test_detect_webdriver():
 
 @pytest.mark.skip(reason="Does not work in test image")
 def test_hide_webdriver():
+    # https://stackoverflow.com/questions/53039551/selenium-webdriver-modifying-navigator-webdriver-flag-to-prevent-selenium-detec
     request = WebDriverRequest("non-automated")
     request.browser = "chrome"
     options = ChromeOptions()
     options.add_argument("--disable-blink-features=AutomationControlled")
     request.options = options
     webdriver = create_webdriver(request)
+    webdriver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     result = webdriver.execute_script("return navigator.webdriver")
     assert result is False
 
