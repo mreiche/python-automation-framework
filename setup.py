f543b39afdac9a71deebcb60ef527e58469cd520
Mike Reiche <mike@reiche.world>
2025-04-16 08:15:24 +0200

    Add Apache 2 license

--- setup.py
+++ setup.py
@@ -10,10 +10,11 @@ setup(
     description="Automation framework for the WebDriver API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.2.2",
+    version="0.2.3",
     url="https://github.com/mreiche/python-automation-framework",
     author="Mike Reiche",
     packages=["paf"],
     install_requires=["inject>=4.3.1", "selenium>=4.23.1", "is-empty>=1.0.1"],
     python_requires=">=3.13",
+    license_files=("LICENSE.txt", )
 )
